8c4b4dfbf39bec3dbc639bfbda9904d44135dab1
muha <muha.ragy@gmail.com>
2023-10-16 06:11:57 +0300

    gg

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -22,7 +22,7 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key:process.env.MIX_PUSHER_APP_KEY,
-    forceTLS: MIX_PUSHER_TLS,
+    forceTLS: process.env.MIX_PUSHER_TLS,
     wsHost: window.location.hostname,
     wsPort: 6001,
     cluster:process.env.MIX_PUSHER_APP_CLUSTER,
